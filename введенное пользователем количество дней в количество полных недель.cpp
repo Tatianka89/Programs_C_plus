0e02442d2f216ae5a710a6a42ebe266e31cbeb9a
TatianaKa89 <abramovich11.06@gmail.com>
2026-07-31 21:49:16 +0300

    Add files via upload

---
+++ введенное пользователем количество дней в количество полных недель.cpp
@@ -0,0 +1,20 @@
+﻿// Написать программу, которая преобразует введенное пользователем количество дней в количество полных недель и оставшихся дней.Например, пользователь ввел 17 дней, программа должна вывести на экран 2 недели и 3 дня.
+//
+#include <iostream>
+using namespace std;
+int main ()
+{
+    const int week = 7; 
+    int days, weeks;
+
+    cout << "days : ";
+    cin >> days; 
+    weeks = days / 7;
+    days = days % 7;
+
+    cout << "weeks : " << weeks << "\n";
+    cout << "days : " << days << "\n";
+    cout << " weeks : " << weeks << " days : " << days * 1 << "\n";
+
+    return 0;
+}
