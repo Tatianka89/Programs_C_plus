043de28f45714e4f74c3d0fa8c51bb7e50ccc485
TatianaKa89 <abramovich11.06@gmail.com>
2026-07-31 21:52:04 +0300

    Add files via upload

---
+++ дробное число в денежный формат.cpp
@@ -0,0 +1,20 @@
+﻿//Написать программу, которая преобразует введенное с клавиатуры дробное число в денежный формат.
+//Например, число 12,5 должно быть преобразовано к виду 12 долларов 50 центов.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double number, dollar, cent;
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите денежную единицу" << endl;
+	cin >> number;
+
+	dollar = int(number);
+	cent = (number - dollar) * 100;
+
+	cout << dollar << " долларов " <<cent << " центов \n";
+	return 0;
+}
