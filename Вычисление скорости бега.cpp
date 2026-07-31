d5504bbef1e5872ead711d5b0b93de1d341de117
TatianaKa89 <abramovich11.06@gmail.com>
2026-07-31 21:53:42 +0300

    Add files via upload

---
+++ Вычисление скорости бега.cpp
@@ -0,0 +1,26 @@
+﻿// Домашняя работа 1.4.1.cpp : Этот файл содержит функцию "main". 
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double speed, distance, time;
+	int minutes, seconds;
+	cout << "Вычисление скорости бега.";
+	cout << " Введите длину дистанции (метров) = ";
+	cin >> distance;
+	cout << " Введите время(мин.сек) = ";
+	cin >> time;
+	minutes = time;
+	seconds = (time - minutes) * 100;
+	time = (minutes * 60) + seconds;
+	speed = distance / time * 3.6;
+	cout << "Дистанция: " << distance << "м\n";
+	cout << "Время: " << minutes << " мин " <<
+		seconds << " сек = " << time << " секунд\n";
+	cout << "Вы бежали со скоростью " << speed << "км/ч\n";
+
+	return 0;
+}
